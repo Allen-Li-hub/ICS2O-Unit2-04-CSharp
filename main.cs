6f96ac94bae0846f7843dd24202a8357ad877d28
Allen-Li-hub <113055658+Allen-Li-hub@users.noreply.github.com>
2022-10-17 18:30:37 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,30 @@
+// Created by: Allen
+// Created on: Oct 2022
+//
+// This program calculates area and perimeter of a triangle
+
+using System;
+
+class Program {
+    public static void Main (string[] args) {
+        // This function accepts user input
+        int baseOfTriangle;
+        int heightOfTriangle;
+        int areaOfTriangle;
+
+        Console.WriteLine("This program calculates the area and perimeter of a triangle.");
+        Console.WriteLine("");
+
+        Console.Write("Enter the base (cm): ");
+        baseOfTriangle = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the height (cm): ");
+        heightOfTriangle = Convert.ToInt32(Console.ReadLine());
+
+        areaOfTriangle = (baseOfTriangle * heightOfTriangle) / 2;
+       
+        Console.WriteLine("");
+        Console.WriteLine("The area is: " + areaOfTriangle + " cm². ");
+
+        Console.WriteLine("\nDone.");
+    }
+}
